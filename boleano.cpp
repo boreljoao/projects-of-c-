cf52b1b2ef0f113592beed43b14f456d4e9437d2
joao pedro borel <128319193+boreljoao@users.noreply.github.com>
2023-08-10 17:17:41 -0300

    Add files via upload

---
+++ boleano.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	bool t1, t2, t3;
+	t1 = (4>=6);//false
+	t2 = true; //true
+	t3 = (true || 5==25/5);
+	cout << t1 << endl;
+	cout << t2 << endl;
+	cout << t3 << endl;
+}
